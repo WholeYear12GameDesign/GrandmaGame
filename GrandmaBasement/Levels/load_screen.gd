7421124cc25dfa2c000f6d737e8ef87b60f52490
WholeYear12GameDesign <147568359+WholeYear12GameDesign@users.noreply.github.com>
2023-10-13 11:27:32 +1300

    Screen Switching

--- GrandmaBasement/Levels/load_screen.gd
+++ GrandmaBasement/Levels/load_screen.gd
@@ -3,15 +3,24 @@ extends Area2D
 @export var next_scene = ""
 @export var offset = Vector2.ZERO
 
-
-#switch to a diffrent screen/section of a dome using the path of the section
+# switch to a diffrent screen/section of a dome using the path of the section
 func switch_screen(path):
 	var scene  = load(path)
-	var instance = scene.instanciate()
-	get_tree().add_child(instance)
+	var instance = scene.instantiate()
+	get_parent().get_parent().add_child(instance)
 	instance.global_position += get_parent().global_position + offset
+	get_parent().get_parent().get_node("Camera2D").global_position = instance.global_position
+	$UnloadTimer.start()
 
-
+# runs switch_screen() when player collides with collision shape
 func _on_body_entered(body):
 	if body.name == "Player":
 		switch_screen(next_scene)
+
+# unload the screen after new one is loaded
+func UnloadTimer():
+	get_parent().queue_free()
+
+# Makes door collidable after some time
+func WaitTimer():
+	monitoring = true
---
+++ GrandmaBasement/Player/Camera2D.gd
@@ -0,0 +1,4 @@
+extends Camera2D
+
+func move_to(target_position):
+	global_position = target_position

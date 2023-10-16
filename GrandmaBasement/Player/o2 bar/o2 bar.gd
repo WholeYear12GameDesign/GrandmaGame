a69b39232d97e656eec5b90dccd0fe984b1a0008
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2023-10-17 11:11:07 +1300

    Improved o2 bar

--- GrandmaBasement/Player/o2 bar/o2 bar.gd
+++ GrandmaBasement/Player/o2 bar/o2 bar.gd
@@ -1,7 +1,12 @@
 extends TextureProgressBar
 
-const grad : Gradient = preload("res://Player/o2 bar/o2 gradient colour.tres").gradient
+const grad : Gradient = preload("res://Player/o2 bar/o2 gradient colour.tres")
 
 func _process(_delta: float) -> void:
-	self_modulate = grad.sample(player_data.oxygen / 100)
-	value = player_data.oxygen
+	var o2 : float = player_data.oxygen
+	self_modulate = grad.sample(o2 / 100)
+	value = o2
+	if o2 < 25:
+		$"../RichTextLabel".text = "[shake rate=25.0 level=5.0][center][b]Oxygen[/b][/center][/shake]"
+	else:
+		$"../RichTextLabel".text = "[center][b]Oxygen[/b][/center]"

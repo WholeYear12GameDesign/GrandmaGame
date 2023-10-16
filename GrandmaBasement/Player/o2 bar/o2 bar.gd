ac13154a7e31f16a3d8a686468a9a093afbb7902
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2023-10-17 12:43:53 +1300

    Improved project settings and fixed o2 bar problems caused by git merge

--- GrandmaBasement/Player/o2 bar/o2 bar.gd
+++ GrandmaBasement/Player/o2 bar/o2 bar.gd
@@ -2,6 +2,11 @@ extends ProgressBar
 
 const grad : Gradient = preload("res://Player/o2 bar/o2 gradient colour.tres")
 
+func _ready() -> void:
+	#if name == "ProgressBar":
+	#	queue_free()
+	pass
+
 func _process(_delta: float) -> void:
 	var o2 : float = player_data.oxygen
 	self_modulate = grad.sample(o2 / 100)

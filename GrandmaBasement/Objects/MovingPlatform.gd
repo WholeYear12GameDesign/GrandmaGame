081a97297ba07ff3e6dcd57e9301d04dac5dab3f
WholeYear12GameDesign <147568359+WholeYear12GameDesign@users.noreply.github.com>
2023-10-19 11:44:55 +1300

    Better Platfor m

--- GrandmaBasement/Objects/MovingPlatform.gd
+++ GrandmaBasement/Objects/MovingPlatform.gd
@@ -1,20 +1,21 @@
 extends Node2D
-# Bottom Left
-@export var pos1 = Vector2.ZERO
-# Top Right
-@export var pos2 = Vector2.ZERO
-@export var speed = 0
-@onready var next_position = pos1
-@onready var starting_pos = position
 
+@export var pos1 = Vector2.ZERO
+@export var pos2 = Vector2.ZERO
+@export var speed = 0.0
+
+var direction = 1
+
+func _ready():
+	$Path2D.curve.add_point(pos1)
+	$Path2D.curve.add_point(pos2)
+	
 func _process(delta):
-	if position.x - starting_pos.x < pos1.x or position.y - starting_pos.y > pos1.y:
-		next_position = pos2
-	elif position.x - starting_pos.x > pos2.x or position.y - starting_pos.y < pos2.y:
-		next_position = pos1
-	print(position - starting_pos)
-	print(next_position)
-	position += next_position * delta * speed
+	if $Path2D/PathFollow2D.progress_ratio <= 0:
+		direction = 1
+	elif $Path2D/PathFollow2D.progress_ratio >= 1:
+		direction = -1
+	$Path2D/PathFollow2D.progress_ratio += direction * delta * speed
 	
 	
 	

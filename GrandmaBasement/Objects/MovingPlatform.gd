52bd9c7836809d2d13668044666477a3dfd4d6e1
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2023-10-20 10:21:05 +1300

    Fixed moving platorm

--- GrandmaBasement/Objects/MovingPlatform.gd
+++ GrandmaBasement/Objects/MovingPlatform.gd
@@ -7,17 +7,18 @@ extends Node2D
 @onready var player = get_tree().get_nodes_in_group("player")[0]
 var enter_pos = Vector2.ZERO
 var direction = 1
-
+var last_position = Vector2.ZERO
 func _ready():
 	$Path2D.curve.add_point(pos1)
 	$Path2D.curve.add_point(pos2)
 	
-func _process(delta):
+func _physics_process(delta):
 	if $Path2D/PathFollow2D.progress_ratio <= 0:
 		direction = 1
 	elif $Path2D/PathFollow2D.progress_ratio >= 1:
 		direction = -1
 	$Path2D/PathFollow2D.progress_ratio += direction * delta * speed
-	print(player)
 	if player in $Path2D/PathFollow2D/PlayerOn.get_overlapping_bodies():
-		player.global_position.x = $Path2D/PathFollow2D/PlayerOn.global_position.x
+		player.global_position.x -= last_position.x - $Path2D/PathFollow2D.global_position.x
+	print(last_position, global_position)
+	last_position = $Path2D/PathFollow2D.global_position
